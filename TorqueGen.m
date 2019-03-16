e1b4c1caf8eb485ec1b6994fde6eefdb23f2875b
Yourong Wang <44557467+Sifyrena@users.noreply.github.com>
2019-03-16 17:06:32 +1300

    Updated with correct math for Jupiter

--- TorqueGen.m
+++ TorqueGen.m
@@ -1,16 +1,17 @@
 % Assuming free space except for external field %
 
-B = 5e-5; % T
+B = 7e-4; % T
 
-N = 1000; % Per m
+N = 100; % Turns 
 
-theta = pi/2; % Alignment
+theta = pi/2; % Alignment Angle. pi/2 being orthogonal.
 
 I = 0.3; % Amps
 
 A = pi * (0.03)^2; % m^2
 
-TorqueGoal = 2.25 * 10^(-5) ;% Nm
+
+TorqueGoal = 2.25 * 10^(-5) ;% Nm. This gives an angular acc. of 5rad/s^2 with an assumed mass of 20g.
 
 
-sin(theta)*N*B*A*I / TorqueGoal
+sin(theta)*N*B*A*I / TorqueGoal % If ~1, we are good.
