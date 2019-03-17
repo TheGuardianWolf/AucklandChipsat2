ee539e83e4667b6ffd1009c9fa3b2cb0ca6c5ece
Jon Pearce <jonpea@gmail.com>
2019-03-17 14:29:26 +1300

    New figure and updated code

--- control_v2.py
+++ control_v2.py
@@ -1,6 +1,17 @@
 import math
 
-def compute(target_global, heading_global, b_target_local):
+def pwm_levels(target_global, heading_global, b_target_local):
+    '''
+    Returns the PWM levels for x- and y- axes.
+    
+    Inputs: See "Figure: Code Explanation" in accompanying submission.
+      target_global: Angle to target in global frame
+      heading_global: Heading angle in global frame
+      b_target_global: Angle of background magnetic field vector in global frame
+
+    Output:
+      x, y: PWM levels for x- and y-coils; sum to 1.0
+    '''
     def global_to_local(angle):
         return angle - heading_global
     heading_local = global_to_local(heading_global)  # identically zero
@@ -11,4 +22,5 @@ def compute(target_global, heading_global, b_target_local):
     y = math.sin(b_generated_local)
     return x, y
 
+# Test that math module import works on the device!
 print(compute(1, 2, 3))
