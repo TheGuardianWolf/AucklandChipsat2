b659db210a345389662a971494a45dfdf18d8ca3
Jon Pearce <jonpea@gmail.com>
2019-03-17 14:13:49 +1300

    After test

--- control_v2.py
+++ control_v2.py
@@ -1,15 +1,14 @@
 import math
-    
-def compute(target_global, heading_global, b_target_local):
 
+def compute(target_global, heading_global, b_target_local):
     def global_to_local(angle):
         return angle - heading_global
-
     heading_local = global_to_local(heading_global)  # identically zero
     target_local = global_to_local(target_global)
     delta = b_target_local - target_local  # b_target_local = target_local + delta
     b_generated_local = heading_local + delta
-    a = b_generated_local  # save ink
-    x = math.cos(a)
-    y = math.sin(a)
+    x = math.cos(b_generated_local)
+    y = math.sin(b_generated_local)
     return x, y
+
+print(compute(1, 2, 3))
